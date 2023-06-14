762156dfb5d1a8023a05801f0203792f775f127b
Valeriia <valeriia.omelchenko1907@gmail.com>
2023-06-15 01:50:54 +0300

    Debug Bluetooth connect

--- SmartLightingSystem/SmartLightingSystem.ino
+++ SmartLightingSystem/SmartLightingSystem.ino
@@ -9,57 +9,92 @@ long nowTime = 0;
 int delayTime = 1000;
 long previousTime = 0;
 int x;
-char data;
+int data;
+bool autoMode = 0;
+byte userBrightness;
+byte minBrightness;
+byte maxBrightness;
+byte brightness;
 
-void lightOn (byte brightness){
-  analogWrite(R,brightness);
-  analogWrite(G,brightness);
-  analogWrite(B,brightness);
-
+void lightOn(byte brightness) {
+  analogWrite(R, brightness);
+  analogWrite(G, brightness);
+  analogWrite(B, brightness);
 }
-void lightOff (){
-  analogWrite(R,0);
-  analogWrite(G,0);
-  analogWrite(B,0);
-}
 
-void autoLighting (byte minL,byte maxL, byte currentLighting){
-  if ( (currentLighting< minL)&&(autoL<255)){
+void autoLighting(byte minL, byte maxL, byte currentLighting) {
+  if ((currentLighting < minL) && (autoL < 255)) {
     autoL++;
     lightOn(autoL);
   }
-    if ( (currentLighting> maxL)&&(autoL>0)){
+  if ((currentLighting > maxL) && (autoL > 0)) {
     autoL--;
     lightOn(autoL);
   }
-  
 }
 
-int photoReader(){
-   nowTime = millis();
+int photoReader() {
+  nowTime = millis();
   if (nowTime - previousTime > delayTime) {
     previousTime = millis();
     x = analogRead(PHOTO);
-    lighting = map(x,1020,100, 0, 100);
+    lighting = map(x, 1020, 100, 0, 100);
   }
-  return lighting;  
+  return lighting;
 }
 void setup() {
   // put your setup code here, to run once:
-pinMode(R,OUTPUT);
-pinMode(G,OUTPUT);
-pinMode(B,OUTPUT);
-Serial.begin(9600);
-Serial3.begin(9600);
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
+  Serial.begin(9600);
+  Serial3.begin(9600);
 }
 
 void loop() {
+  if (Serial3.available()) {
+    data = Serial3.parseInt();
+  }
+
+if (data == 505) {
+      autoMode = 0;
+    } else if (data == 404) {
+      autoMode = 1;
+    } else if (data == 303) {
+      Serial3.print(photoReader());
+    } else if (data < 101 && autoMode == 1) {
+      userBrightness = data;
+      minBrightness = userBrightness - 5;
+      maxBrightness = userBrightness + 5;
+      autoLighting(minBrightness, maxBrightness, photoReader());
+    } else if (data < 101 && autoMode == 0) {
+      brightness = map(data, 0, 100, 0, 255);
+      lightOn(brightness);
+    }
   // put your main code here, to run repeatedly:
-/*Serial.print(photoReader());
-Serial.print("\t");
-Serial.println(autoL);*/
+  Serial.print("Auto flag: ");
+  Serial.print(autoMode);
+  Serial.print("\t");
 
-autoLighting(50,60,photoReader());
+  Serial.print("Fhotoresistor value: ");
+  Serial.print(photoReader());
+  Serial.print("\t");
 
-delay(100);
+Serial.print("Bluetooth value: ");
+  Serial.println(data);
+   Serial.print("\t");
+
+   Serial.print("User`s brightness of LED: ");
+  Serial.print(userBrightness);
+  Serial.print("\t");
+
+  Serial.print("Current auto brightness of LED: ");
+  Serial.print(autoL);
+  Serial.print("\t");
+
+Serial.print("Current brightness of LED: ");
+  Serial.print(brightness);
+  Serial.print("\t");
+  delay(100);
+  // autoLighting(50, 60, photoReader());
 }
